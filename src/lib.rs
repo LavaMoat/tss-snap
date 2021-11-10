fa1f8f54c036e8877739ba059198fc5a2bd59178
muji <muji@tmpfs.org>
2021-11-10 08:39:37 +0800

    Set up logging and panic hook.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,24 @@
-use multi_party_ecdsa::*;
+use wasm_bindgen::prelude::*;
+//use multi_party_ecdsa::*;
 
+#[cfg(target_arch = "wasm32")]
+extern crate wasm_bindgen;
 
-fn lib() {
-    todo!()
+#[cfg(all(test, target_arch = "wasm32"))]
+extern crate wasm_bindgen_test;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    console_error_panic_hook::set_once();
+    console_log!("WASM: module started.");
 }
