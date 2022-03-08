f48aa93fa5aa2ebd4615df800b3a959a5f012e9b
muji <muji@tmpfs.org>
2022-03-08 12:30:53 +0800

    Improve documentation for common module.

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,10 +1,15 @@
 /// Common types shared between the server and webassembly modules.
 use serde::{Deserialize, Serialize};
 
-/// Parameters for key generation and signing.
+/// Parameters used during key generation.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Parameters {
+    /// Number of parties `n`.
     pub parties: u16,
+    /// Threshold for signing `t`.
+    ///
+    /// The threshold must be crossed (`t + 1`) for signing
+    /// to commence.
     pub threshold: u16,
 }
 
