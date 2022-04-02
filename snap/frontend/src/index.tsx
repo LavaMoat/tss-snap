167de53d99a296f7515bb3fed66cd73f59d30568
muji <muji@tmpfs.org>
2022-04-02 10:57:23 +0800

    Fix type checking for ethereum global.

--- snap/frontend/src/index.tsx
+++ snap/frontend/src/index.tsx
@@ -1,7 +1,20 @@
-import React, { useState, useContext, useEffect } from "react";
+import React from "react";
 import * as ReactDOMClient from 'react-dom/client';
 
 import App from './app';
+
+type RpcRequest = {
+  method: string;
+  params?: any;
+}
+
+type SnapEthereum = {
+  request: (req: RpcRequest) => Promise<unknown>;
+}
+
+declare global {
+  const ethereum: SnapEthereum;
+}
 
 const root = ReactDOMClient.createRoot(document.querySelector("main"));
 root.render(
