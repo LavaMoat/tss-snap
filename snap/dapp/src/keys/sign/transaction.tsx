bee0e46a3467fd374dd9b0a6ce656d221f230043
muji <muji@tmpfs.org>
2022-11-18 14:27:42 +0800

    Prepare RLP encoding of transaction.

--- snap/dapp/src/keys/sign/transaction.tsx
+++ snap/dapp/src/keys/sign/transaction.tsx
@@ -12,9 +12,9 @@ import {chains} from '../../utils';
 
 import SelectedChain from '../selected-chain';
 
-import { utils, UnsignedTransaction } from 'ethers';
+import { utils, UnsignedTransaction, BigNumber } from 'ethers';
 
-function TransactionForm() {
+function TransactionForm({chain, gasPrice, transactionCount}) {
   const [address, setAddress] = useState("");
   const [addressError, setAddressError] = useState(false);
 
@@ -36,23 +36,59 @@ function TransactionForm() {
       const to = utils.getAddress(address);
       try {
         const value = utils.parseUnits(amount);
+
+        const gasLimit = "0x30d400";
+        const data = "0x00";
+        // NOTE: Must do some conversion so that
+        // NOTE: RLP encoding works as expected, otherwise
+        // NOTE: some values are not detected as BytesLike
+        const chainId = BigNumber.from(chain)
+        const nonce = BigNumber.from(transactionCount)
+        //const maxFeePerGas = "";
+        //const maxPriorityFeePerGas = "";
+
         const transaction: UnsignedTransaction = {
+          nonce: nonce.toHexString(),
           to,
           value,
+          gasPrice,
+          gasLimit,
+          data,
+          //maxFeePerGas,
+          //maxPriorityFeePerGas,
+          chainId: chainId.toHexString(),
         };
 
         console.log("tx", transaction);
+
+        const txParams = [
+          transaction.nonce,
+          transaction.gasPrice,
+          transaction.gasLimit,
+          transaction.to,
+          transaction.value.toHexString(),
+          transaction.data,
+          transaction.chainId,
+          '0x00',
+          '0x00'
+        ];
+
+        console.log("params", txParams);
+
+        const rawTransaction = utils.RLP.encode(txParams);
+        console.log(rawTransaction.toString('hex'));
+
+        const transactionHash = utils.keccak256(rawTransaction);
+        console.log(transactionHash.toString('hex'));
+
       } catch (e) {
         setAmountError(true)
       }
-
     } catch (e) {
       setAddressError(true)
     }
 
   };
-
-
 
   /*
         <Stack marginBottom={1}>
@@ -106,6 +142,8 @@ export default function SignTransaction() {
   const [selectedParty, setSelectedParty] = useState(null);
 
   const [balance, setBalance] = useState("0x0");
+  const [gasPrice, setGasPrice] = useState("0x0");
+  const [transactionCount, setTransactionCount] = useState("0x0");
   const [chain, setChain] = useState<string>(null);
 
   useEffect(() => {
@@ -120,6 +158,17 @@ export default function SignTransaction() {
         method: "eth_chainId",
       })) as string;
       setChain(chainId);
+
+      const gasPrice = (await ethereum.request({
+        method: "eth_gasPrice",
+      })) as string;
+      setGasPrice(gasPrice)
+
+      const transactionCount = (await ethereum.request({
+        method: "eth_getTransactionCount",
+        params: [address, "latest"],
+      })) as string;
+      setTransactionCount(transactionCount)
 
       const result = (await ethereum.request({
         method: "eth_getBalance",
@@ -183,15 +232,17 @@ export default function SignTransaction() {
 
         <Stack
           direction="row"
-          alignItems="center"
-          justifyContent="spaceBetween">
+          alignItems="center">
 
-          <Box>{utils.formatEther(balance)} ETH</Box>
+          <Box sx={{flexGrow: 1}}>{utils.formatEther(balance)} ETH</Box>
           <SelectedChain chainName={chainName} />
 
         </Stack>
 
-        <TransactionForm />
+        <TransactionForm
+          chain={chain}
+          gasPrice={gasPrice}
+          transactionCount={transactionCount} />
 
       </Stack>
     </>
