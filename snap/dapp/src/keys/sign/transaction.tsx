cb381d9112c3382ef9a3ad95ec1d5f92e2314bf4
muji <muji@tmpfs.org>
2022-11-18 10:27:03 +0800

    Prepare transaction form.

--- snap/dapp/src/keys/sign/transaction.tsx
+++ snap/dapp/src/keys/sign/transaction.tsx
@@ -1,16 +1,70 @@
-import React from "react";
+import React, {useState} from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-import { Breadcrumbs, Link, Stack, Typography } from "@mui/material";
+import { Box, Chip, Breadcrumbs, Link, Stack, TextField, Typography } from "@mui/material";
 
 import NotFound from "../../not-found";
 import PublicAddress from "../../components/public-address";
 import { keysSelector } from "../../store/keys";
+import { sessionSelector } from "../../store/session";
+
+function TransactionForm() {
+
+  const [address, setAddress] = useState("");
+  const [addressError, setAddressError] = useState(false);
+
+  const [amount, setAmount] = useState("");
+  const [amountError, setAmountError] = useState(false);
+
+  const onAddressChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setAddress(e.target.value);
+
+  const onAmountChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setAmount(e.target.value);
+
+  return (
+    <Stack spacing={2}>
+      <Stack marginBottom={1}>
+        <Typography variant="body1" component="div">
+          Enter the address to send to.
+        </Typography>
+        <Typography variant="body2" component="div" color="text.secondary">
+          Should be a valid public address starting with 0x.
+        </Typography>
+      </Stack>
+
+      <TextField
+        label="Address"
+        autoFocus
+        autoComplete="off"
+        onChange={onAddressChange}
+        value={address}
+        error={addressError}
+        variant="outlined"
+        placeholder="Enter the address for the recipient"
+      />
+
+      <TextField
+        label="Amount"
+        autoComplete="off"
+        onChange={onAmountChange}
+        value={amount}
+        error={amountError}
+        variant="outlined"
+        placeholder="Amount of ETH to send"
+      />
+
+    </Stack>
+  );
+}
 
 export default function SignTransaction() {
   const { address } = useParams();
   const { keyShares } = useSelector(keysSelector);
+  const { signProof } = useSelector(sessionSelector);
+  const [selectedParty, setSelectedParty] = useState(null);
+
   const keyShare = keyShares.find((item) => {
     const [keyAddress] = item;
     return keyAddress === address;
@@ -20,8 +74,19 @@ export default function SignTransaction() {
     return <NotFound />;
   }
 
-  const share = keyShare[1];
-  const { label } = share;
+  const keyShareGroup: KeyShareGroup = keyShare[1];
+  const { label, threshold, parties, items } = keyShareGroup;
+
+  const selectedKeyShare =
+    signProof === null ? (
+      <>
+        <Box sx={{ flexGrow: 1 }} />
+        <Chip
+          label={`Using key share for party #${selectedParty || items[0]}`}
+        />
+      </>
+    ) : null;
+
   return (
     <>
       <Stack spacing={2}>
@@ -39,7 +104,15 @@ export default function SignTransaction() {
             {label}
           </Typography>
         </Stack>
-        <PublicAddress address={address} abbreviate />
+
+        <Stack direction="row" alignItems="center">
+          <PublicAddress address={address} abbreviate />
+          {selectedKeyShare}
+        </Stack>
+
+
+        <TransactionForm />
+
       </Stack>
     </>
   );
