539a9dbc4fbb60a6c482e9c65f9e28c7f25437a9
muji <muji@tmpfs.org>
2022-11-23 14:31:55 +0800

    Add fund-account.js script.

---
+++ snap/dapp/fund-account.js
@@ -0,0 +1,25 @@
+const ethers = require('ethers');
+const { utils } = ethers;
+
+async function fundAccount(privateKey, to, amount) {
+  const provider = new ethers.providers.JsonRpcProvider();
+  const wallet = new ethers.Wallet(privateKey, provider);
+  //const balance = await provider.getBalance(to);
+  //const nonce = await provider.getTransactionCount(to);
+  //const value = utils.parseUnits(amount);
+  const tx = {
+    to,
+    value: utils.parseEther(amount)
+  };
+  const result = await wallet.sendTransaction(tx);
+  console.log(result);
+}
+
+// Private key from Ganache, usually at index zero
+const privateKey = "0xf64abc91d673bcf100c3cf2bc42507df566d36a18189ae41c377c55ee26a44fd";
+
+// Address we want to fund - the MPC address
+const to = "0x93afae63c135aa9a50f0fdc574d9f9f3a4c35d33";
+
+// Send 1 ETH
+fundAccount(privateKey, to, "1.0");
