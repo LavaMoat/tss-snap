b0cfc72c04b4bd512b37d89e006ede85c08c03fd
muji <muji@tmpfs.org>
2022-11-29 16:02:33 +0800

    Update to address for fund account script.

--- snap/dapp/fund-account.js
+++ snap/dapp/fund-account.js
@@ -19,8 +19,9 @@ async function fundAccount(privateKey, to, amount) {
 const from = "0xD928Bb02420d5b4E50AdFD3ca9567d60C6590550";
 const privateKey = "0xf64abc91d673bcf100c3cf2bc42507df566d36a18189ae41c377c55ee26a44fd";
 
+
 // Address we want to fund - the MPC address
-const to = "0xd97fcd0a87a76c4e1b5f173b3dc2c9764ab03906";
+const to = "0xbc37a5b4e51eb49993d5a24aded7c1e382e8f680";
 
 // Send 1 ETH
 fundAccount(privateKey, to, "10.0");
